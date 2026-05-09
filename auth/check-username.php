c00147bd2276b641227f750edb3c81f43187dd30
Vince Kobe <cvincekobe@gmail.com>
2026-05-09 12:25:51 +0800

    add CORS to auth files

--- auth/check-username.php
+++ auth/check-username.php
@@ -1,5 +1,20 @@
 <?php
 
+$allowed_origins = [
+    'http://localhost:3000',
+    'https://city-pet-vaccination-frontend.vercel.app',
+    'https://petunityph.vercel.app'
+];
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+if (in_array($origin, $allowed_origins)) {
+    header("Access-Control-Allow-Origin: $origin");
+} else {
+    header("Access-Control-Allow-Origin: https://petunityph.vercel.app");
+}
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header('Content-Type: application/json; charset=UTF-8');
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
