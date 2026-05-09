900cf4b2f5905abb7101f3071d2e7f971359ec0a
Vince Kobe <cvincekobe@gmail.com>
2026-05-09 09:37:48 +0800

    fix sql mode for railway

--- config/database.php
+++ config/database.php
@@ -55,11 +55,11 @@ $dsn = "mysql:host=" . $config['host'] . ";port=" . $config['port'] . ";dbname="
             
             // PDO options for better security and performance
             $options = [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
-            ];
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4; SET SESSION sql_mode = ''"
+];
             
             // Create PDO connection
             $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
